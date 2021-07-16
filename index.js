f8e8f337ea369a47c104a6ee276dd2f3952ba26f
jamesboblak <james@skepticalrecords.com>
2021-07-15 23:52:33 -0700

    added first user prompt question to index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the name of your project',
+      name: 'name',
+    },
+
+  ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
